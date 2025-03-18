cba6734df1339968232d2f1d3aaf6e7ba09d7525
Ishimine <felipecalafate@hotmail.com>
2025-03-18 11:01:21 -0300

    sss

---
+++ Scripts/VisualElementExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public static class VisualElementExtensions
+{
+    public static async Awaitable WaitForPress(this VisualElement element,CancellationToken token)
+    {
+        AwaitableCompletionSource completionSource = new AwaitableCompletionSource();
+        element.RegisterCallbackOnce<PointerDownEvent>(_=> completionSource.TrySetResult());
+        try
+        {
+            await completionSource.Awaitable;
+        }
+        finally
+        {
+            completionSource.TrySetCanceled();
+        }
+    }
+    
+}
