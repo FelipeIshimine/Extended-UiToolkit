54384774691009d8ab64e774525b1550a4712ab2
Ishimine <felipecalafate@hotmail.com>
2025-03-28 12:47:36 -0300

    HoverColorManipulator

---
+++ Scripts/Manipulators/HoverColorManipulator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HoverColorManipulator : MouseManipulator
+{
+    private readonly Color hoverColor;
+    private readonly Color normalColor;
+    
+    public HoverColorManipulator(Color hoverColor, Color normalColor)
+    {
+        this.hoverColor = hoverColor;
+        this.normalColor = normalColor;
+    }
+
+    protected override void RegisterCallbacksOnTarget()
+    {
+        target.RegisterCallback<MouseEnterEvent>(OnMouseEnter);
+        target.RegisterCallback<MouseLeaveEvent>(OnMouseLeave);
+    }
+
+    protected override void UnregisterCallbacksFromTarget()
+    {
+        target.UnregisterCallback<MouseEnterEvent>(OnMouseEnter);
+        target.UnregisterCallback<MouseLeaveEvent>(OnMouseLeave);
+    }
+
+    private void OnMouseEnter(MouseEnterEvent evt)
+    {
+        (target as VisualElement).style.color = hoverColor;
+    }
+
+    private void OnMouseLeave(MouseLeaveEvent evt)
+    {
+        (target as VisualElement).style.color = normalColor;
+    }
+}
